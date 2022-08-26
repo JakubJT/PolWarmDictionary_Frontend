76b8e14ffd4dfda090dcfb2f18714b3c248f8001
Jakub T <gopi@op.pl>
2022-08-26 13:53:27 +0200

    Searching words works

---
+++ Models/Word.cs
@@ -0,0 +1,7 @@
+namespace Frontend.Models;
+public class Word
+{
+    public string? InWarmian { get; set; }
+    public string? InPolish { get; set; }
+    public string? PartOfSpeech { get; set; }
+}
