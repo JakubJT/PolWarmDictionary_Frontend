a981d0ff291744dd087bbf8cca993db1fa02ed97
Jakub T <gopi@op.pl>
2022-08-30 11:13:40 +0200

    Word editing function added

--- Models/Word.cs
+++ Models/Word.cs
@@ -1,6 +1,7 @@
 namespace Frontend.Models;
 public class Word
 {
+    public int Id { get; set; }
     public string? InWarmian { get; set; }
     public string? InPolish { get; set; }
     public string? PartOfSpeech { get; set; }
