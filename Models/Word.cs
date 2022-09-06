86707e231d9973ddf609abf5381412c7bb0bcfa3
Jakub T <gopi@op.pl>
2022-09-06 12:35:38 +0200

    Validation added

--- Models/Word.cs
+++ Models/Word.cs
@@ -1,8 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Frontend.Models;
 public class Word
 {
     public int Id { get; set; }
+
+    private const string ValidationMessageWarmian = "Pole \"Po warmińsku\" powinno zawierać od 1 do 16 liter";
+    private const string ValidationMessagePolish = "Pole \"Po polsku\" powinno zawierać od 1 do 16 liter";
+    private const string ValidationMessageOnlyLetters = "Teskt powinnien zaczynać się z wielkiej litery i zawierać tylko litery";
+
+    [Required]
+    [StringLength(16, ErrorMessage = ValidationMessageWarmian)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = ValidationMessageOnlyLetters)]
     public string? InWarmian { get; set; }
+
+    [Required]
+    [StringLength(16, ErrorMessage = ValidationMessagePolish)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = ValidationMessageOnlyLetters)]
     public string? InPolish { get; set; }
+
+    [Required]
     public string? PartOfSpeech { get; set; }
 }
