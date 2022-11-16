5462eb2bcd48ff7efba764cb1f31fd75e6a2c236
Jakub T <jakub@tyllo.pl>
2022-11-16 14:44:46 +0100

    Word sorting logic moved to a separate class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<Endpoint>();
+builder.Services.AddTransient<Sorting>();
 builder.Services.AddMudServices();
 builder.Services.AddSessionStorageServices();
 
---
+++ Sorting.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace PolWarmDictionary_Frontend;
+
+public class Sorting
+{
+    private bool ascendingOrderInPolish { get; set; } = true;
+    private bool ascendingOrderInWarmian { get; set; } = false;
+    private bool ascendingOrderPartOfSpeech { get; set; } = false;
+    public Sorting(IWebAssemblyHostEnvironment hostEnvironment, IConfiguration configuration)
+    {
+
+    }
+
+    public (string, bool) Sort(string sortBy, (string SortBy, bool AscendingOrder) sortOptions)
+    {
+        sortOptions.SortBy = sortBy;
+        switch (sortBy)
+        {
+            case "InPolish":
+                if (ascendingOrderInPolish)
+                {
+                    sortOptions.AscendingOrder = false;
+                    ascendingOrderInPolish = false;
+                    return sortOptions;
+                }
+                else
+                {
+                    sortOptions.AscendingOrder = true;
+                    ascendingOrderInPolish = true;
+
+                    ascendingOrderInWarmian = false;
+                    ascendingOrderPartOfSpeech = false;
+
+                    return sortOptions;
+                }
+            case "InWarmian":
+                if (ascendingOrderInWarmian)
+                {
+                    sortOptions.AscendingOrder = false;
+                    ascendingOrderInWarmian = false;
+                    return sortOptions;
+                }
+                else
+                {
+                    sortOptions.AscendingOrder = true;
+                    ascendingOrderInWarmian = true;
+
+                    ascendingOrderInPolish = false;
+                    ascendingOrderPartOfSpeech = false;
+                    return sortOptions;
+                }
+            case "PartOfSpeech":
+                if (ascendingOrderPartOfSpeech)
+                {
+                    sortOptions.AscendingOrder = false;
+                    ascendingOrderPartOfSpeech = false;
+                    return sortOptions;
+                }
+                else
+                {
+                    sortOptions.AscendingOrder = true;
+                    ascendingOrderPartOfSpeech = true;
+
+                    ascendingOrderInPolish = false;
+                    ascendingOrderInWarmian = false;
+                    return sortOptions;
+                }
+            default:
+                return sortOptions;
+        }
+    }
+}
