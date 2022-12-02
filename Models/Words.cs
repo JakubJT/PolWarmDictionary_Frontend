a693fe1b113e7b10bf31a26e916611fccb2ad8fa
Jakub T <jakub@tyllo.pl>
2022-12-02 20:46:20 +0100

    Refactoring of pagination component

--- Models/Words.cs
+++ Models/Words.cs
@@ -2,5 +2,5 @@ namespace PolWarmDictionary_Frontend.Models;
 public class Words
 {
     public List<Word>? WordList { get; set; }
-    public int NumbeOfPages { get; set; }
+    public int NumberOfPages { get; set; }
 }
