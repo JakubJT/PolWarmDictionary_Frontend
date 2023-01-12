4def206eeeb904b22a764677c36082936af32dfa
Jakub T <jakub@tyllo.pl>
2023-01-12 20:38:41 +0100

    Users page added; small cleaning

--- Pages/WordGroups.razor.js
+++ Pages/WordGroups.razor.js
@@ -1,5 +1,17 @@
+var theModal;
+
 export function showActionToast() {
     var actionToast = document.getElementById('actionToast')
     var toast = new bootstrap.Toast(actionToast)
     toast.show()
+};
+
+export function showModal() {
+    var modal = new bootstrap.Modal(document.getElementById('createCollectionModal'));
+    modal.show();
+    theModal = modal;
+};
+
+export function hideModal() {
+    theModal.hide();
 };
